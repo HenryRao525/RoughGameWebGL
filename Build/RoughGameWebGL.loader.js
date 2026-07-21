6731c9828578a2fb714a64e0c0af0de134235498
HenryRao525 <henry2006114@icloud.com>
2026-07-21 16:15:34 +0800

    Better UI and Event system

--- Build/RoughGameWebGL.loader.js
+++ Build/RoughGameWebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 55896489,
+    wasmFileSize: 55920399,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
