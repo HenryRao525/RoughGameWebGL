6c2616b8be3f6f5e7b995ff6ea24c7fec3e8ef23
HenryRao525 <henry2006114@icloud.com>
2026-07-24 03:18:24 +0800

    emergency fix

--- Build/RoughGameWebGL.loader.js
+++ Build/RoughGameWebGL.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 56221703,
+    wasmFileSize: 56222558,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
